8da8666b127a88863cbbd2c9f9b37ba90551928d
Leulseged Bekele <88849889+Leulbkm@users.noreply.github.com>
2022-11-24 02:26:44 +0300

    implement delete

---
+++ php_action/delete_brand.php
@@ -0,0 +1,20 @@
+<?php
+include_once 'connect.php';
+$valid['success'] = array('success' => false, 'messages' => array());
+$id = $_GET['id'];
+if ($id){
+    $sql = "DELETE FROM `brands` WHERE `brand_id` ='$id'";
+    $result = $conn->query($sql);
+    if($conn->query($sql) === TRUE) {
+		$valid['success'] = true;
+		$valid['messages'] = "Successfully Deleted";	
+	} else {
+		$valid['success'] = false;
+		$valid['messages'] = "Error while Deleting the members";
+	}
+    $conn->close();
+    
+    echo json_encode($valid);
+    header('Location: ../brand.php');
+}
+?>
